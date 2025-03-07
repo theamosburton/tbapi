58c58e951726a956b8076fbc5842874f93be5c63
Amos Burton <amosbrton1207@gmail.com>
2025-03-07 10:20:34 -0600

    ip changed

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const fs = require("fs");
+const https = require("https");
+const express = require("express");
+
 const app = express();
 
-// Middleware to parse JSON body
-app.use(express.json());
+// Load Self-Signed SSL certificate
+const options = {
+  key: fs.readFileSync("/etc/ssl/certs/selfsigned.key"),
+  cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+};
 
 app.post('/create', (req, res) => {
     console.log(req.body);
     res.status(200).json({ walletaddes: true, message: "" });
 });
 
-const PORT = 443;
-
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+// Start HTTPS server
+https.createServer(options, app).listen(443, () => {
+  console.log("Server running on https://YOUR_PUBLIC_IP");
 });
