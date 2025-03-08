589de987d8c702dcaaf451b8d3393577e7107ad1
Amos Burton <amosbrton1207@gmail.com>
2025-03-08 10:06:37 -0600

    gfdgdf

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.post('/create', (req, res) => {
     res.status(200).json({ status: "true", message: "hello" });
 });
 
+app.get('/create', (req, res) => {
+    console.log("📥 Data Received:", req.body);
+    res.status(200).json({ status: "true", message: "hello" });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
