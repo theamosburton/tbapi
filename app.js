74804e91d9a0c0ca543a568edb0133e23a13e5c8
Amos Burton <amosbrton1207@gmail.com>
2025-03-08 09:59:19 -0600

    fgfdgdfg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3241;
 app.use(express.urlencoded({ extended: true }));
 app.post('/create', (req, res) => {
-    console.log("📥 Data Received:", req);
+    console.log("📥 Data Received:", req.body);
     res.status(200).json({ status: "true", message: "hello" });
 });
 
